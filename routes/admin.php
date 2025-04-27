781501f6cf776c7c3ab7121f5dc8e235f36c9cca
Leonardo Daniel Cruz Rosas <tudiopsss@hotmail.com>
2025-04-26 18:42:08 -0600

    Se quitaron Facades que no se usaban del archivo admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -4,9 +4,6 @@
 use App\Http\Controllers\admin\AvisoController;
 use Illuminate\Support\Facades\Route;
 
-use App\Imports\AvisosImport;
-
-use Illuminate\Http\Request;
 
 
 Route::resource('admin/avisos', AvisoController::class)->names('admin.avisos')->middleware('auth');
